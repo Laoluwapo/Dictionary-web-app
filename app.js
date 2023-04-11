c0d7dddcc6960240e36610558d261d3c596c258b
laoluwapo <alividegabriel9@gmail.com>
2023-04-11 20:04:42 +0100

    Set the values for the search-term section from the API.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const searchInput = document.querySelector(".search input");
 const emptyErrorInput = document.querySelector(".error-empty");
 const sound = document.querySelector(".sound");
+const result = document.querySelector(".result-section");
+const playBtn = document.querySelector(".play-icon");
 
 // Functions
 // Function that fetches the data from the API
@@ -18,6 +20,8 @@ const getUserData = async (e) => {
         }
         const data = await response.json();
         console.log(data);
+        // Show the result section
+        result.style.display = "block";
         // Clear search input
         searchInput.value = "";
         return data;
@@ -34,8 +38,31 @@ const getUserData = async (e) => {
 async function searchMeaning(e) {
   try {
     const data = await getUserData(e);
+    setValues(data);
   } catch (error) {
     console.error(error.message);
+  }
+}
+
+function setValues(data) {
+  const searchedWord = document.querySelector(".search-term h1");
+  const phoneticsText = document.querySelector(".search-term p");
+  if (data && data.length > 0) {
+    searchedWord.textContent = data[0].word;
+    const phonetics = data[0].phonetics;
+    if (phonetics && phonetics.length > 0 && phonetics[0].audio) {
+      phoneticsText.textContent = phonetics[0].text;
+      sound.setAttribute("src", phonetics[0].audio);
+      playBtn.disabled = false;
+      playBtn.classList.remove("disabled");
+      playBtn.addEventListener("click", () => {
+        sound.play();
+      });
+    } else {
+      sound.setAttribute("src", "");
+      playBtn.disabled = true;
+      playBtn.classList.add("disabled");
+    }
   }
 }
 
